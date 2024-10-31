55a69458c27f081542419240fdfa4b6b027cd08e
YardenEz13 <yarden.etz@gmail.com>
2024-10-31 10:59:03 +0200

    הוספת פונקציונליות התחברות והרשמה למשתמשים

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 const { z }=require('zod');
-const  { signUpSchema}= require('../lib/validation/user');
+const  { signUpSchema,signInSchema}= require('../lib/validation/user');
 const User =require('../models/user')
 const bcrypt= require('bcrypt');
 const jwt= require('jsonwebtoken');
@@ -27,21 +27,8 @@ const signUp= async(req,res)=>{
        if(!newUser){
         return res.status(400).json({message:'Failed to create user'});
        }
-      const token= jwt.sign(
-        {
-            id:newUser._id,
-            username:newUser.username,
-        },
-        process.env.JWT_SECRET,
-        {expiresIn:'24h'}
-      );
-      res.cookie('token',token,
-        {
-            httpOnly:true,
-            samesite:'strict',
-            maxAge:24*60*60*1000,
-        });
-       return res.status(201).json({message:'User created successfully',token});
+       setTokenCookie(res,newUser,process.env.JWT_SECRET);
+       return res.status(201).json({message:'User created successfully'});
     }
     catch(error){
         console.log(error);
@@ -52,7 +39,28 @@ const signUp= async(req,res)=>{
         return res.status(500).json({ message:'internal server error'});
     }
 };
-
+const signIn= async(req,res)=>{
+try{
+    const{username,password}=signInSchema.parse(req.body);
+        const user= await User.findOne({username});
+        if(!user){
+            return res.status(400).json({message:'Invalid username or password'});
+        }
+        const passwordMatch= await bcrypt.compare(password,user.password);
+        if(!passwordMatch){
+            return res.status(400).json({message:'Invalid username or password'});
+        }
+        setTokenCookie(res,user,process.env.JWT_SECRET);
+        return res.status(200).json({message:'Sign in successful'});
+}
+catch(error){
+    console.log(error);
+}
+if(error instanceof z.ZodError){
+    return res.status(400).json({message:error.errors[0].message});
+}
+return res.status(500).json({message:'internal server error'});
+}
 module.exports={
-    signUp,
+    signUp,signIn
 }
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,7 @@
 const { signUp } = require("../controllers/user");
 const express = require("express");
 const router = express.Router();
-
-
-
+router.post("/sign-in",signIn);
 router.post("/sign-up", signUp);
 
 module.exports = router;
