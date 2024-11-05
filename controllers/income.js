b4ac9d8a29ca5763256675f9787924038fbf4e40
YardenEz13 <yarden.etz@gmail.com>
2024-11-05 12:43:42 +0200

    add income validation and controller

--- controllers/income.js
+++ controllers/income.js
@@ -5,12 +5,13 @@ const {z}= require('zod');
 const addIncome= async(req,res)=>{
     try{
         const userId=userIdValidation.parse(req.params.userId);
+        const {title,description,amount, tag, currency}= req.body;
+        
         const userExists= await User.findById(userId);
         if(!userExists){
             return res.status(404).json({message:'User not found'});
         }
-        const {amount,description,date}= req.body;
-        const newIncome= new Income({amount,description,date,userId});
+        const newIncome= new Income({title,description,amount,tag,currency,userId});
         await newIncome.save();
         return res.status(200).json({message:'Income added successfully'});
     }
