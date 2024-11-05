009fa1a1548d1e3e12b6e3b23be54f673d64c5fd
YardenEz13 <yarden.etz@gmail.com>
2024-11-05 11:57:30 +0200

    hhhhh

--- controllers/income.js
+++ controllers/income.js
@@ -1,8 +1,16 @@
 const Income= require('../models/income');
+const User= require('../models/user');
 const {z}= require('zod');
 const addIncome= async(req,res)=>{
     try{
         const userId= req.params.userId;
+        const userExists= await User.findById(userId);
+        if(!userExists){
+            return res.status(404).json({message:'User not found'});
+        }
+        const {amount,description,date}= req.body;
+        const newIncome= new Income({amount,description,date,userId});
+        await newIncome.save();
         return res.status(200).json({message:'Income added successfully'});
     }
     catch(error){
