16025b80495a232981239081a2dd1341eb5ebb00
YardenEz13 <yarden.etz@gmail.com>
2024-11-05 13:11:11 +0200

    add income validation and controller

--- controllers/income.js
+++ controllers/income.js
@@ -2,17 +2,20 @@ const { userIdValidation } = require('../lib/validation/user');
 const Income= require('../models/income');
 const User= require('../models/user');
 const {z}= require('zod');
+const {incomeSchema,incomeIdValidation}= require('../lib/validation/income');
 const addIncome= async(req,res)=>{
     try{
         const userId=userIdValidation.parse(req.params.userId);
-        const {title,description,amount, tag, currency}= req.body;
+        const {title,description,amount, tag, currency}= incomeSchema.parse(req.body);
         
         const userExists= await User.findById(userId);
         if(!userExists){
             return res.status(404).json({message:'User not found'});
         }
-        const newIncome= new Income({title,description,amount,tag,currency,userId});
-        await newIncome.save();
+        const income= new Income({title,description,amount,tag,currency});
+        await income.save();
+        userExists.incomes.push(income);
+        await userExists.save();
         return res.status(200).json({message:'Income added successfully'});
     }
     catch(error){
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const userRouter = require('./routes/user')
+const routes= require('./routes');
 const connectDB = require('./lib/connect');
+
 
 
 const app = express();
 app.use(express.json());
-app.use(userRouter);
+app.use(routes);
 
 console.log(process.env.DATABASE_URL)
 
